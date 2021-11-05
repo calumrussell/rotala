eae9a46345aa2b1e9984f39862cbc9f2c1469373
Calum Russell <calum.mj.russell@gmail.com>
2021-11-05 03:39:40 +0000

    adding tighter data structures around common operations

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -83,3 +83,145 @@ impl DefaultDataSource {
         DefaultDataSource { data, pos: 0, keys }
     }
 }
+
+pub struct TimeSeries {
+    index: Vec<f64>,
+    values: Vec<f64>,
+}
+
+impl TimeSeries {
+    pub fn pct_change_log(&self) -> Vec<f64> {
+        let mut res: Vec<f64> = Vec::new();
+        let mut temp = &self.values[0];
+        for i in self.values.iter().skip(1).into_iter() {
+            let pct_change = i / temp;
+            res.push(pct_change.log10());
+            temp = i
+        }
+        res
+    }
+
+    pub fn pct_change(&self) -> Vec<f64> {
+        let mut res: Vec<f64> = Vec::new();
+        let mut temp = &self.values[0];
+        for i in self.values.iter().skip(1).into_iter() {
+            res.push((i / temp) - 1.0);
+            temp = &i;
+        }
+        res
+    }
+
+    pub fn maxdd(&self) -> f64 {
+        let mut maxdd = 0.0;
+        let mut peak = 0.0;
+        let mut trough = 0.0;
+        let mut t2 = 0.0;
+
+        for t1 in &self.values {
+            if t1 > &peak {
+                peak = t1.clone();
+                trough = peak;
+            } else if t1 < &trough {
+                trough = t1.clone();
+                t2 = (trough / peak) - 1.0;
+                if t2 < maxdd {
+                    maxdd = t2
+                }
+            }
+        }
+        maxdd
+    }
+
+    pub fn count(&self) -> usize {
+        self.values.len()
+    }
+
+    pub fn var(&self) -> f64 {
+        let mean: f64 = self.values.iter().sum::<f64>() / (self.count() as f64);
+        let squared_diffs = self
+            .values
+            .iter()
+            .map(|ret| ret - mean)
+            .map(|diff| diff.powf(2.0))
+            .collect_vec();
+        let sum_of_diff = squared_diffs.iter().sum::<f64>();
+        sum_of_diff / (self.count() as f64)
+    }
+
+    pub fn vol(&self) -> f64 {
+        //Accepts returns not raw portfolio values
+        self.var().sqrt()
+    }
+
+    pub fn append(&mut self, idx: Option<f64>, value: f64) {
+        if idx.is_some() {
+            self.index.push(idx.unwrap());
+            self.values.push(value);
+        } else {
+            let idx_last = self.index.last();
+            if idx_last.is_none() {
+                self.index.push(0.0);
+            } else {
+                self.index.push(idx_last.unwrap() + 1.0);
+            }
+            self.values.push(value)
+        }
+    }
+
+    pub fn new(index: Option<Vec<f64>>, values: Vec<f64>) -> Self {
+        if index.is_some() {
+            TimeSeries {
+                index: index.unwrap(),
+                values,
+            }
+        } else {
+            if values.len() == 0 {
+                TimeSeries { index: Vec::new(), values }
+            } else {
+                let idx = (0..values.len() - 1).map(|v| v as f64).collect_vec();
+                TimeSeries { index: idx, values }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::TimeSeries;
+
+    fn setup() -> TimeSeries {
+        let mut fake_prices: Vec<f64> = Vec::new();
+        fake_prices.push(100.0);
+        fake_prices.push(105.0);
+        fake_prices.push(120.0);
+        fake_prices.push(80.0);
+        fake_prices.push(90.0);
+        TimeSeries::new(None, fake_prices)
+    }
+
+    #[test]
+    fn test_that_returns_calculates_correctly() {
+        let ts = setup();
+        let rets = ts.pct_change();
+        let sum = rets.iter().map(|v| (1.0 + v).log10()).sum::<f64>();
+        let val = (10_f64.powf(sum) - 1.0) * 100.0;
+        assert_eq!(val.round(), -10.0)
+    }
+
+    #[test]
+    fn test_that_vol_calculates_correctly() {
+        let ts = setup();
+        let rets = TimeSeries::new(None, ts.pct_change());
+        assert_eq!((rets.vol() * 100.0).round(), 19.0);
+
+        let log_rets = TimeSeries::new(None, ts.pct_change_log());
+        assert_eq!((log_rets.vol() * 100.0).round(), 10.0);
+    }
+
+    #[test]
+    fn test_that_mdd_calculates_correctly() {
+        let ts = setup();
+        let mdd = ts.maxdd();
+        assert_eq!((mdd * 100.0).round(), -33.0);
+    }
+}
--- src/perf/mod.rs
+++ src/perf/mod.rs
@@ -1,8 +1,8 @@
 use itertools::Itertools;
 
 use crate::broker::{CashManager, PositionInfo};
+use crate::data::TimeSeries;
 use crate::portfolio::PortfolioStats;
-use crate::trading;
 
 enum DataFrequency {
     Daily,
@@ -10,71 +10,9 @@ enum DataFrequency {
     Yearly,
 }
 
-struct PortfolioSnapshot {
-    value: f64,
-}
-
 pub struct PortfolioCalculator;
 
 impl PortfolioCalculator {
-    fn get_log_returns(values: &Vec<f64>) -> Vec<f64> {
-        let mut res: Vec<f64> = Vec::new();
-        let mut temp = &values[0];
-        for i in values.iter().skip(1).into_iter() {
-            let pct_change = i / temp;
-            res.push(pct_change.log10());
-            temp = i
-        }
-        res
-    }
-
-    fn get_returns(values: &Vec<f64>) -> Vec<f64> {
-        let mut res: Vec<f64> = Vec::new();
-        let mut temp = values[0];
-        for i in values.iter().skip(1).into_iter() {
-            res.push((i / temp) - 1.0);
-            temp = i.clone();
-        }
-        res
-    }
-
-    fn get_maxdd(values: &Vec<f64>) -> f64 {
-        let mut maxdd = 0.0;
-        let mut peak = 0.0;
-        let mut trough = 0.0;
-        let mut t2 = 0.0;
-
-        for t1 in values {
-            if t1 > &peak {
-                peak = t1.clone();
-                trough = peak;
-            } else if t1 < &trough {
-                trough = t1.clone();
-                t2 = (trough / peak) - 1.0;
-                if t2 < maxdd {
-                    maxdd = t2
-                }
-            }
-        }
-        maxdd * 100.0
-    }
-
-    fn get_variance(values: &Vec<f64>) -> f64 {
-        let mean: f64 = values.iter().sum::<f64>() / (values.len() as f64);
-        let squared_diffs = values
-            .iter()
-            .map(|ret| ret - mean)
-            .map(|diff| diff.powf(2.0))
-            .collect_vec();
-        let sum_of_diff = squared_diffs.iter().sum::<f64>();
-        sum_of_diff / (values.len() as f64)
-    }
-
-    fn get_volatility(returns: &Vec<f64>) -> f64 {
-        //Accepts returns not raw portfolio values
-        (PortfolioCalculator::get_variance(returns).sqrt()) * 100.0
-    }
-
     fn annualize_returns(ret: f64, trading_days: Option<i32>, frequency: DataFrequency) -> f64 {
         let mut days = 0.0;
         if trading_days.is_none() {
@@ -105,26 +43,13 @@ impl PortfolioCalculator {
 }
 
 pub struct PortfolioPerformance {
-    history: Vec<PortfolioSnapshot>,
-    values: Option<Vec<f64>>,
+    portfolio_value: TimeSeries,
 }
 
 impl PortfolioPerformance {
-    fn to_values(&mut self) -> &Vec<f64> {
-        if self.values.is_none() {
-            let values = self
-                .history
-                .iter()
-                .map(|snap| -> f64 { snap.value })
-                .collect_vec();
-            self.values = Some(values);
-        }
-        self.values.as_ref().unwrap()
-    }
-
     pub fn get_portfolio_volatility(&mut self) -> f64 {
-        let rets = PortfolioCalculator::get_returns(self.to_values());
-        PortfolioCalculator::get_volatility(&rets)
+        let rets = TimeSeries::new(None, self.portfolio_value.pct_change());
+        rets.vol()
     }
 
     pub fn get_portfolio_sharpe_ratio(&mut self) -> f64 {
@@ -134,27 +59,22 @@ impl PortfolioPerformance {
     }
 
     pub fn get_portfolio_max_dd(&mut self) -> f64 {
-        PortfolioCalculator::get_maxdd(self.to_values())
+        self.portfolio_value.maxdd()
     }
 
     pub fn get_portfolio_return(&mut self) -> f64 {
-        let sum_log_rets = PortfolioCalculator::get_log_returns(&self.to_values())
-            .iter()
-            .sum();
+        let sum_log_rets = self.portfolio_value.pct_change_log().iter().sum();
         (10_f64.powf(sum_log_rets) - 1.0) * 100.0
     }
 
     pub fn update(&mut self, port: &impl PortfolioStats, brkr: &(impl PositionInfo + CashManager)) {
         let value = port.get_total_value(brkr);
-        let snap = PortfolioSnapshot { value };
-        self.history.push(snap);
+        self.portfolio_value.append(None, value);
     }
 
     pub fn new() -> Self {
-        let history: Vec<PortfolioSnapshot> = Vec::new();
         PortfolioPerformance {
-            history,
-            values: None,
+            portfolio_value: TimeSeries::new(None, Vec::new()),
         }
     }
 }
@@ -163,45 +83,6 @@ impl PortfolioPerformance {
 mod tests {
     use super::DataFrequency;
     use super::PortfolioCalculator;
-
-    fn setup() -> Vec<f64> {
-        let mut fake_prices: Vec<f64> = Vec::new();
-        fake_prices.push(100.0);
-        fake_prices.push(105.0);
-        fake_prices.push(120.0);
-        fake_prices.push(80.0);
-        fake_prices.push(90.0);
-        fake_prices
-    }
-
-    #[test]
-    fn test_that_returns_calculates_correctly() {
-        let values = setup();
-        let rets = PortfolioCalculator::get_returns(&values);
-        let sum = rets.iter().map(|v| (1.0 + v).log10()).sum::<f64>();
-        let val = (10_f64.powf(sum) - 1.0) * 100.0;
-        assert!(val.round() == -10.0)
-    }
-
-    #[test]
-    fn test_that_vol_calculates_correctly() {
-        let values = setup();
-        let returns = PortfolioCalculator::get_returns(&values);
-        let vol = PortfolioCalculator::get_volatility(&returns);
-
-        let log_returns = PortfolioCalculator::get_log_returns(&values);
-        let log_vol = PortfolioCalculator::get_volatility(&log_returns);
-
-        assert!(vol.round() == 19.0);
-        assert!(log_vol.round() == 10.0)
-    }
-
-    #[test]
-    fn test_that_mdd_calculates_correctly() {
-        let values = setup();
-        let mdd = PortfolioCalculator::get_maxdd(&values);
-        assert!(mdd.round() == -33.0);
-    }
 
     #[test]
     fn test_that_annualizations_calculate_correctly() {
