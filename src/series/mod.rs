d6a7b812cff3bd9db058e2cc770713e01453406d
Calum <calum.mj.russell@gmail.com>
2022-09-15 00:47:20 +0100

    deleted TimeSeries struct/impl, replaced with stateless functions over generic Series

--- src/series/mod.rs
+++ src/series/mod.rs
@@ -1,53 +1,51 @@
 use itertools::Itertools;
 
-#[derive(Clone)]
-pub struct TimeSeries {
-    index: Vec<f64>,
-    values: Vec<f64>,
-}
+/// Low level functions that apply over lists or matrices of numbers.
+/// No state. No information relating to the specifics of backtests should
+/// be stored here.
+/// 
+/// Values current hard-coded to f64 due to the prevalance of log operations.
 
-impl TimeSeries {
-    pub fn get_values(&self) -> Vec<f64> {
-        self.values.clone()
-    }
+//Initial impl of Series had state relating to backtest that results in
+//confusing code relating to periodicity that repeated close elsewhere
+//in the package. Minimal state, just operations on lists.
+pub struct Series;
 
-    pub fn get_index(&self) -> Vec<f64> {
-        self.index.clone()
-    }
+impl Series {
 
-    pub fn pct_change_log(&self) -> Vec<f64> {
+    pub fn pct_change_log(values: Vec<f64>) -> Vec<f64> {
         let mut res: Vec<f64> = Vec::new();
-        let mut temp = &self.values[0];
-        for i in self.values.iter().skip(1) {
-            let pct_change = *i / *temp;
-            res.push(pct_change.log10());
-            temp = i
+        let mut temp = values[0];
+        for i in values.iter().skip(1) {
+            let pct_change = *i / temp;
+            res.push(pct_change.ln());
+            temp = *i
         }
         res
     }
 
-    pub fn pct_change(&self) -> Vec<f64> {
+    pub fn pct_change(values: Vec<f64>) -> Vec<f64> {
         let mut res: Vec<f64> = Vec::new();
-        let mut temp = &self.values[0];
-        for i in self.values.iter().skip(1) {
+        let mut temp = values[0];
+        for i in values.iter().skip(1) {
             res.push((i / temp) - 1.0);
-            temp = i;
+            temp = *i;
         }
         res
     }
 
-    pub fn maxdd(&self) -> f64 {
+    pub fn maxdd(values: Vec<f64>) -> f64 {
         let mut maxdd = 0.0;
         let mut peak = 0.0;
         let mut trough = 0.0;
         let mut t2;
 
-        for t1 in &self.values {
-            if t1 > &peak {
-                peak = *t1;
+        for t1 in values {
+            if t1 > peak {
+                peak = t1;
                 trough = peak;
-            } else if t1 < &trough {
-                trough = *t1;
+            } else if t1 < trough {
+                trough = t1;
                 t2 = (trough / peak) - 1.0;
                 if t2 < maxdd {
                     maxdd = t2
@@ -57,96 +55,43 @@ impl TimeSeries {
         maxdd
     }
 
-    pub fn count(&self) -> usize {
-        self.values.len()
-    }
-
-    pub fn var(&self) -> f64 {
-        let mean: f64 = self.values.iter().sum::<f64>() / (self.count() as f64);
-        let squared_diffs = self
-            .values
+    pub fn var(values: Vec<f64>) -> f64 {
+        let count = values.len();
+        let mean: f64 = values.iter().sum::<f64>() / (count as f64);
+        let squared_diffs = values
             .iter()
             .map(|ret| ret - mean)
             .map(|diff| diff.powf(2.0))
             .collect_vec();
         let sum_of_diff = squared_diffs.iter().sum::<f64>();
-        sum_of_diff / (self.count() as f64)
+        sum_of_diff / (count as f64)
     }
 
-    pub fn vol(&self) -> f64 {
+    pub fn vol(values: Vec<f64>) -> f64 {
         //Accepts returns not raw portfolio values
-        self.var().sqrt()
+        Series::var(values).sqrt()
     }
 
-    pub fn get(&self, idx: usize) -> Option<&f64> {
-        self.values.get(idx)
-    }
-
-    pub fn append(&mut self, idx: Option<f64>, value: f64) {
-        if let Some(idx_val) = idx {
-            self.index.push(idx_val);
-            self.values.push(value);
-        } else {
-            let mut last = 0.0;
-            if let Some(idx_last) = self.index.last() {
-                last = *idx_last;
-            }
-            self.index.push(last);
-            self.values.push(value);
-        }
-    }
-
-    pub fn new<T: Into<f64>>(index: Option<Vec<T>>, values: Vec<T>) -> Self {
-        let mut val_converted: Vec<f64> = Vec::new();
-        for v in values {
-            let val = v.into();
-            val_converted.push(val);
-        }
-
-        if let Some(index_val) = index {
-            let mut idx_converted: Vec<f64> = Vec::new();
-            for i in index_val {
-                let val = i.into();
-                idx_converted.push(val);
-            }
-
-            TimeSeries {
-                index: idx_converted,
-                values: val_converted,
-            }
-        } else if val_converted.is_empty() {
-            TimeSeries {
-                index: Vec::new(),
-                values: Vec::new(),
-            }
-        } else {
-            let idx = (0..val_converted.len() - 1).map(|v| v as f64).collect_vec();
-            TimeSeries {
-                index: idx,
-                values: val_converted,
-            }
-        }
-    }
-}
+} 
 
 #[cfg(test)]
 mod tests {
-    use super::TimeSeries;
+    use super::Series;
 
-    fn setup() -> TimeSeries {
+    fn setup() -> Vec<f64> {
         let mut fake_prices: Vec<f64> = Vec::new();
         fake_prices.push(100.0);
         fake_prices.push(105.0);
         fake_prices.push(120.0);
         fake_prices.push(80.0);
         fake_prices.push(90.0);
-        TimeSeries::new(Option::None, fake_prices)
+        fake_prices
     }
 
     #[test]
     fn test_that_returns_calculates_correctly() {
         let ts = setup();
-        let rets = ts.pct_change();
+        let rets = Series::pct_change(ts);
         let sum = rets.iter().map(|v| (1.0 + v).log10()).sum::<f64>();
         let val = (10_f64.powf(sum) - 1.0) * 100.0;
         assert_eq!(val.round(), -10.0)
@@ -155,17 +100,17 @@ mod tests {
     #[test]
     fn test_that_vol_calculates_correctly() {
         let ts = setup();
-        let rets = TimeSeries::new(None, ts.pct_change());
-        assert_eq!((rets.vol() * 100.0).round(), 19.0);
+        let rets = Series::pct_change(ts.clone());
+        assert_eq!((Series::vol(rets) * 100.0).round(), 19.0);
 
-        let log_rets = TimeSeries::new(None, ts.pct_change_log());
-        assert_eq!((log_rets.vol() * 100.0).round(), 10.0);
+        let log_rets = Series::pct_change_log(ts);
+        assert_eq!((Series::vol(log_rets) * 100.0).round(), 22.0);
     }
 
     #[test]
     fn test_that_mdd_calculates_correctly() {
         let ts = setup();
-        let mdd = ts.maxdd();
+        let mdd = Series::maxdd(ts);
         assert_eq!((mdd * 100.0).round(), -33.0);
     }
 }
