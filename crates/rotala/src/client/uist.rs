3d367029dc83b769b40b1e5e17b01b5c18a39022
Calum <calum.mj.russell@gmail.com>
2024-01-04 10:16:58 +0000

    fmt

--- crates/rotala/src/client/uist.rs
+++ crates/rotala/src/client/uist.rs
@@ -1,6 +1,6 @@
 //! Client implements an Exchange API over the network so that the interface called is the same
 //! on the network as locally.
-//! 
+//!
 //! Uses reqwest to send requests to server.
 use reqwest::Result;
 
