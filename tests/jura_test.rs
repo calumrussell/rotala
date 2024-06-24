fc41c79a3367b0bb4e3f27716c45bd15888a0cac
Calum <>
2024-06-24 21:44:51 +0100

    fix binaries, fix tets

--- tests/jura_test.rs
+++ tests/jura_test.rs
@@ -1,11 +1,13 @@
-use rotala::exchange::jura_v1::{random_jura_generator, Order};
+use rotala::input::penelope::Penelope;
+use rotala::exchange::jura_v1::{JuraV1, Order};
 
 #[test]
 fn test_that_uist_works() {
-    let (mut exchange, _clock) = random_jura_generator(1000);
-
-    let _init = exchange.init();
+    let source = Penelope::random(1000);
+    let mut exchange = JuraV1::new();
 
     let order = Order::market_buy(0, "100.0", "97.00");
     exchange.insert_order(order);
+
+    exchange.tick(source.get_quotes_unchecked(&100));
 }
