2b3aba8f6b8179db3f03028d2144d48484b3a5da
Calum <>
2024-06-25 10:46:04 +0100

    benches working

--- rotala/benches/sim_orderbook.rs
+++ rotala/benches/sim_orderbook.rs
@@ -1,31 +1,27 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
 use rotala::exchange::uist_v1::{Order, UistV1};
-use rotala::input::penelope::PenelopeBuilder;
+use rotala::input::penelope::Penelope;
 
 fn uist_core_loop_test() {
-    let mut source_builder = PenelopeBuilder::new();
-    source_builder.add_quote(100.00, 101.00, 100, "ABC");
-    source_builder.add_quote(10.00, 11.00, 100, "BCD");
-    source_builder.add_quote(100.00, 101.00, 101, "ABC");
-    source_builder.add_quote(10.00, 11.00, 101, "BCD");
-    source_builder.add_quote(104.00, 105.00, 102, "ABC");
-    source_builder.add_quote(10.00, 11.00, 102, "BCD");
-    source_builder.add_quote(104.00, 105.00, 103, "ABC");
-    source_builder.add_quote(12.00, 13.00, 103, "BCD");
+    let mut source = Penelope::new();
+    source.add_quote(100.00, 101.00, 100, "ABC");
+    source.add_quote(10.00, 11.00, 100, "BCD");
+    source.add_quote(100.00, 101.00, 101, "ABC");
+    source.add_quote(10.00, 11.00, 101, "BCD");
+    source.add_quote(104.00, 105.00, 102, "ABC");
+    source.add_quote(10.00, 11.00, 102, "BCD");
+    source.add_quote(104.00, 105.00, 103, "ABC");
+    source.add_quote(12.00, 13.00, 103, "BCD");
 
-    let mut uist = UistV1::from_penelope_builder(
-        &mut source_builder,
-        "Fake",
-        rotala::clock::Frequency::Second,
-    );
+    let mut uist = UistV1::new();
 
     uist.insert_order(Order::market_buy("ABC", 100.0));
     uist.insert_order(Order::market_buy("ABC", 100.0));
 
-    uist.tick();
-    uist.tick();
-    uist.tick();
+    uist.tick(source.get_quotes_unchecked(&100));
+    uist.tick(source.get_quotes_unchecked(&101));
+    uist.tick(source.get_quotes_unchecked(&102));
 }
 
 fn benchmarks(c: &mut Criterion) {
