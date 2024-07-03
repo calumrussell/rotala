3904a7f06f05b77cc20fab789640b66e0027b989
Calum <calum.mj.russell@gmail.com>
2024-07-03 19:38:01 +0100

    faulty exchange impl for MarketBuy/MarketSell

--- rotala/src/exchange/uist_v2.rs
+++ rotala/src/exchange/uist_v2.rs
@@ -1,15 +1,37 @@
 use std::collections::VecDeque;
 
+use anyhow::Result;
 use serde::{Deserialize, Serialize};
+
+use crate::input::athena::Depth;
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Quote {
+    pub bid: f64,
+    pub bid_volume: f64,
+    pub ask: f64,
+    pub ask_volume: f64,
+    pub date: i64,
+    pub symbol: String,
+}
+
+impl From<crate::input::athena::BBO> for Quote {
+    fn from(value: crate::input::athena::BBO) -> Self {
+        Self {
+            bid: value.bid,
+            bid_volume: value.bid_volume,
+            ask: value.ask,
+            ask_volume: value.ask_volume,
+            date: value.date,
+            symbol: value.symbol,
+        }
+    }
+}
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Deserialize, Serialize)]
 pub enum OrderType {
     MarketSell,
     MarketBuy,
-    LimitSell,
-    LimitBuy,
-    StopSell,
-    StopBuy,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -37,4 +59,74 @@ pub struct Trade {
 
 pub struct OrderBook {
     inner: VecDeque<Order>,
+    last_inserted: u64,
+}
+
+impl Default for OrderBook {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl OrderBook {
+    pub fn new() -> Self {
+        Self {
+            inner: std::collections::VecDeque::new(),
+            last_inserted: 0,
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.inner.is_empty()
+    }
+
+    fn trade_loop(depth: &Depth, order: &Order) -> Option<Trade> {
+        let best = depth.get_bbo()?;
+        match order.order_type {
+            OrderType::MarketBuy => {
+                if order.price.unwrap() >= best.ask {
+                    Some(Trade {
+                        symbol: order.symbol.clone(),
+                        value: best.ask * order.qty,
+                        quantity: order.qty,
+                        date: depth.date,
+                        typ: TradeType::Buy,
+                    })
+                } else {
+                    None
+                }
+            }
+            OrderType::MarketSell => {
+                if order.price.unwrap() <= best.bid {
+                    Some(Trade {
+                        symbol: order.symbol.clone(),
+                        value: best.bid * order.qty,
+                        quantity: order.qty,
+                        date: depth.date,
+                        typ: TradeType::Sell,
+                    })
+                } else {
+                    None
+                }
+            }
+        }
+    }
+
+    pub fn execute_orders(&mut self, quotes: crate::input::athena::DateQuotes) -> Vec<Trade> {
+        let mut trade_results = Vec::new();
+        if self.is_empty() {
+            return trade_results;
+        }
+
+        for order in self.inner.iter() {
+            let security_id = &order.symbol;
+
+            if let Some(depth) = quotes.get(security_id) {
+                if let Some(trade) = Self::trade_loop(depth, order) {
+                    trade_results.push(trade);
+                }
+            }
+        }
+        trade_results
+    }
 }
