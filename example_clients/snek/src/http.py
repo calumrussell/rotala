149ef39afb34a3a3c31b700448f0ecdab6719c56
Calum Russell <calum@Calums-MacBook-Pro.local>
2024-09-06 14:05:47 +0100

    added test client for http client

--- example_clients/snek/src/http.py
+++ example_clients/snek/src/http.py
@@ -1,4 +1,6 @@
 import requests
+import json
+from functools import wraps
 
 
 class HttpClient:
@@ -32,3 +34,49 @@ class HttpClient:
     def now(self, backtest_id):
         r = requests.get(f"{self.base_url}/backtest/{backtest_id}/now")
         return r.json()
+
+
+def json_response(inner_func):
+    @wraps(inner_func)
+    def wrapper(*args, **kwargs):
+        return json.dumps(inner_func(*args, **kwargs))
+
+    return wrapper
+
+
+class TestHttpClient:
+    def __init__(self):
+        pass
+
+    @json_response
+    def tick(self, backtest_id):
+        return {
+            "has_next": False,
+            "executed_trades": [],
+            "insert_orders": [],
+        }
+
+    @json_response
+    def insert_order(self, order, backtest_id):
+        return {}
+
+    @json_response
+    def fetch_quotes(self, backtest_id):
+        return {
+            "quotes": [],
+        }
+
+    @json_response
+    def init(self, dataset_name):
+        return {"backtest_id": 0}
+
+    @json_response
+    def info(self, backtest_id):
+        return {
+            "version": "",
+            "dataset": "",
+        }
+
+    @json_response
+    def now(self, backtest_id):
+        return {"now": 0, "has_next": True}
